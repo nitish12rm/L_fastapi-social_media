e772cc52fd3c37acb9c0a817133bba17a7e52ab1
nitish <nitish12rm@gmail.com>
2023-09-21 15:32:10 +0530

    hmm

--- alembic/versions/eee5f41a95f4_add_foregin_key_to_post_table.py
+++ alembic/versions/eee5f41a95f4_add_foregin_key_to_post_table.py
@@ -20,7 +20,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade():
     op.add_column('posts',sa.Column('owner_id',sa.Integer(),nullable=False)),
-    op.create_foreign_key('post_user_fk',source_table="posts",referent_table="users",local_cols=["owner_id"],remote_cols=['id'],ondelete="CASCADE")
+    op.create_foreign_key('post_user_fk',source_table="posts",referent_table="users",local_cols=["owner_id"],remote_cols=["id"],ondelete="CASCADE")
     pass
 
 
